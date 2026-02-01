583cccb7031b7e86cd353a6388dcd867fb31edc7
Svyatoslav20 <m68405990@gmail.com>
2026-02-01 11:23:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,10 +14,22 @@ function createBoard(columns, count) {
     board.append(gameTable)
     board.append(restartBtn)
     for(i = 0; i < count; i++) { gameTable.append(createCard())}
+    
+    let icons = createIconsArray(inpVal * inpVal)
+    icons.forEach((icon) => {
+        gameTable.append(createCard(icon));
+    });
 }
-function createCard() {
+function createCard(flippedIcon) {
     let templCard = document.querySelector('#cardTemplate').cloneNode(true).content
     let card = templCard.querySelector('.card')
+
+    const flippedIconElement = card.querySelector('#flippedIcon')
+    if (flippedIconElement && flippedIcon) {
+        flippedIconElement.classList.add(`fa-${flippedIcon}`)
+    }
+    
+    return card
 }
 restartBtn.addEventListener('click', () =>{
     location.reload()
@@ -47,4 +59,32 @@ startInp.addEventListener('input', () => {
     if(inputValue >= 6) {startInp.value = maxVal}
     else if(inputValue <= 2) {startInp.value = minVal}
 })
-createCard(4, 4)
+function createIconsArray(initialCount) {
+    let cardsIcons = [
+        "compass", "cloud", "play", "bolt", "stop", "cogs", "atom",
+        "basketball-ball", "arrows", "angle-left", "bars", "file", 
+        "filter", "gear", "folder", "folder-open", "shield", "scissors", "pen-clip"
+    ]
+    let cards = cardsIcons.slice(0, Math.floor(initialCount / 2))      
+    let doubleCards = dublicateElements(cards)
+    return shuffleArray(doubleCards) 
+}
+function dublicateElements(array) {
+    let massive = [];
+    array.forEach((item) => {
+        massive.push(item, item)
+    })
+    return massive;
+}
+function shuffleArray(array) {
+    let shuffled = [...array]
+    let currentIndex = array.length
+    while(currentIndex > 0) {
+        currentIndex--;
+        const randomIndex = Math.floor(Math.random() * currentIndex)
+        const temp = array[randomIndex]
+        shuffled[currentIndex] = shuffled[randomIndex]
+        shuffled[randomIndex] = temp
+    }
+    return shuffled
+}
--- settings.js
+++ settings.js
@@ -1,10 +1,72 @@
+const firColor = document.querySelector('#first')
+const secColor = document.querySelector('#second')
+const thdColor = document.querySelector('#third')
+const fouthColor = document.querySelector('#fourth')
+const fifColor =document.querySelector('#fifth')
 // const board = document.querySelector('.divS')
 // const startBtn = document.querySelector('.startBtn')
 // const startInp = document.querySelector('.startInp')
 const setBtn = document.querySelector('.settingsBtn')
 const setMenu = document.querySelector('.settingsMenu')
+const setInput = document.querySelector('#setInput')
+const body = document.querySelector('body')
+const makerBtn = document.querySelector('.maker')
 function OpenSettingsMenu() {
-    console.warn('Поздравляю, вы открыли настройки!')
     setMenu.classList.toggle('active')
 }
-setBtn.addEventListener('click', OpenSettingsMenu)
+
+function bgColorChange() {
+    const example = document.querySelector('.square')
+    let inpValue = setInput.value || 120
+    let bgColor = {
+        one :firColor.value,
+        two :secColor.value,
+        three: thdColor.value,
+        four: fouthColor.value,
+        five: fifColor.value,
+    }
+    body.style.background = `linear-gradient(${inpValue}deg, ${bgColor.one} 0%, ${bgColor.two} 25%,  ${bgColor.three} 50%,  ${bgColor.four} 75%,  ${bgColor.five} 100%)`
+    body.style.animation = `bgAnimation 120s ease infinite`
+    body.style.backgroundSize = `200% 200%`
+
+    example.style.background =  `linear-gradient(${inpValue}deg, ${bgColor.one} 0%, ${bgColor.two} 25%,  ${bgColor.three} 50%,  ${bgColor.four} 75%,  ${bgColor.five} 100%)`;
+    
+}
+document.addEventListener('mousemove', e => {
+    const settingsImg = document.querySelector('.settingsImg')
+    const setBtn = document.querySelector('.settingsBtn')
+    const rect = settingsImg.getBoundingClientRect(); // координата
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    if(mouseX > rect.left -100 && mouseX < rect.right + 100 && mouseY > rect.top - 100 && mouseY < rect.bottom + 100) {
+        settingsImg.style.paddingLeft = '40px'
+        setBtn.style.animation = 'none'
+        settingsImg.style.opacity = '.85'
+    } else {
+        settingsImg.style.paddingLeft = '25px'
+        setBtn.style.animation = 'waitMe 10s ease infinite'
+        settingsImg.style.opacity = '1'    
+    }
+})
+function addEventListener() {
+    setBtn.addEventListener('click', OpenSettingsMenu) // старт-закрытие меню
+
+    firColor.addEventListener('input', bgColorChange) // ColorInput
+    secColor.addEventListener('input', bgColorChange) // ColorInput
+    thdColor.addEventListener('input', bgColorChange) // ColorInput
+    fouthColor.addEventListener('input', bgColorChange) // ColorInput
+    fifColor.addEventListener('input', bgColorChange) // ColorInput
+    setInput.addEventListener('input', bgColorChange) // ColorInput
+}
+addEventListener()
+
+let maker = true // темная тема текста - true
+function themeMaker() { // тема текста
+    maker = !maker
+    if(maker) {
+        body.style.color = 'black'
+    }  else {
+        body.style.color = '#ffffffe8'
+    }
+}
+makerBtn.addEventListener('click', themeMaker)
